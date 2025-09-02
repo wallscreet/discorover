56fae942abfb3c56ac3c0aa4a553bb534aa1d71a
wallscreet <wallscreet@gmail.com>
2025-09-02 16:19:47 -0500

    add lucide icon separators

--- src/app/commodity-prices/page.tsx
+++ src/app/commodity-prices/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import dynamic from "next/dynamic";
+import { ChartCandlestick } from "lucide-react";
 
 // Avoid SSR for recharts
 const CommodityPriceChart = dynamic(
@@ -10,11 +11,26 @@ const CommodityPriceChart = dynamic(
 
 export default function Page() {
   return (
-    <main className="mx-auto max-w-6xl px-4 py-8 pt-18 lg:pt-20">
-      <h1 className="text-xl font-semibold text-center mb-4">Commodity Prices — Nominal vs Real</h1>
-      <p className="text-center px-10">
-        This chart shows the price of everyday commodities over time, adjusted for inflation. You can use the dropdown to switch between different items and see both the nominal (actual) price and the real price in today&apos;s dollars side by side. It&apos;s a simple way to track how costs have really changed, beyond the original price tag.
-      </p>
+    <main className="mx-auto max-w-4xl px-4 py-8 pt-18 lg:pt-20">
+      
+      <div className="gap-6">       
+        <h1 className="text-2xl font-semibold text-center mb-4 text-center">
+            Commodity Prices — Nominal vs Real
+        </h1>
+      </div>
+
+      <div>
+          <div className="flex items-center my-2 mb-4">
+              <div className="flex-grow border-t border-gray-300"></div>
+                  <ChartCandlestick className="mx-4 text-gray-400 w-4 h-4" />
+              <div className="flex-grow border-t border-gray-300"></div>
+          </div>
+          
+          <p className="text-center px-10 text-md mb-6">
+              This chart shows the price of everyday commodities over time, adjusted for inflation. You can use the dropdown to switch between different items and see both the nominal (actual) price and the real price in today&apos;s dollars side by side. It&apos;s a simple way to track how costs have really changed, beyond the original price tag.
+          </p>
+          <div className="h-px bg-gradient-to-r from-transparent via-gray-400 to-transparent"></div>
+      </div>
 
       <CommodityPriceChart />
     </main>
--- src/app/home-affordability/page.tsx
+++ src/app/home-affordability/page.tsx
@@ -1,29 +1,32 @@
 'use client'
 
 import PastAffordabilityCard from "@/components/Affordability";
-import { CircleDollarSign } from "lucide-react";
+import { House } from "lucide-react";
 
 export default function Page() {
   return (
-    <main className="mx-auto max-w-6xl px-4 py-8 pt-18 lg:pt-20">
+    <main className="mx-auto max-w-4xl px-4 py-8 pt-18 lg:pt-20">
+        
+        <div className="gap-6">       
+            <h1 className="text-2xl font-semibold text-center mb-4 text-center">
+                Home Affordability by Year
+            </h1>
+        </div>
 
-        <div className="">
-            <div>
-              <h1 className="text-xl font-semibold text-center mb-4 text-[#0d122c]">Home Affordability by Year</h1>
-                <div className="flex items-center my-2 mb-4">
-                    <div className="flex-grow border-t border-gray-300"></div>
-                        <CircleDollarSign className="mx-4 text-gray-400 w-4 h-4" />
-                    <div className="flex-grow border-t border-gray-300"></div>
-                </div>
-                
-                <p className="text-center px-10 text-md mb-4">
-                    Track U.S. home affordability from 1971 through 2023. Most of the data is sourced from FRED. Homeowner&apos;s insurance premiums are estimated by scaling the Property and Casualty PPI to 2023 national averages and adjusting for inflation. The Principal + Interest + Insurance (PII) ratio shows how much of a typical family&apos;s income would go toward owning a home each year, helping to illustrate real trends in housing costs over time.
-                </p>
-                <div className="h-px bg-gradient-to-r from-transparent via-gray-400 to-transparent"></div>
+        <div>
+            <div className="flex items-center my-2 mb-4">
+                <div className="flex-grow border-t border-gray-300"></div>
+                    <House className="mx-4 text-gray-400 w-4 h-4" />
+                <div className="flex-grow border-t border-gray-300"></div>
             </div>
+            
+            <p className="text-center px-10 text-md mb-6">
+                Track U.S. home affordability from 1971 through 2023. Most of the data is sourced from FRED. Homeowner&apos;s insurance premiums are estimated by scaling the Property and Casualty PPI to 2023 national averages and adjusting for inflation. The Principal + Interest + Insurance (PII) ratio shows how much of a typical family&apos;s income would go toward owning a home each year, helping to illustrate real trends in housing costs over time.
+            </p>
+            <div className="h-px bg-gradient-to-r from-transparent via-gray-400 to-transparent"></div>
         </div>
         
-        <div className="rounded-xl p-6 shadow-xl">
+        <div className="rounded-xl p-6 shadow-xl mt-2">
             <PastAffordabilityCard/>
         </div>
 
